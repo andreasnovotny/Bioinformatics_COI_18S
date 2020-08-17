e8b8a353a3d1599008aef7c591eb1dd024178cfa
morien <evan.morien@gmail.com>
2020-08-17 12:28:27 -0700

    removed taxa file from input + associated code

--- collapse_OTU_table.py
+++ collapse_OTU_table.py
@@ -14,17 +14,17 @@ import numpy as np
 #it's important to make sure your closed-reference OTU picking that takes place before running this script is as stringent as you would like it to be, or you will lose a lot of detail/granularity of OTUs present in your original MED output.
 
 ##inputs:
-###1. MED output MATRIX-COUNTS.txt
-###2. Accession/OTU map from closed ref OTU picking output
-###3. Complete taxonomy assignments for accessions/OTUs
+###1. dada2 output sequence_table.myexperiment.txt #tab-delimited sequence x sample matrix, sequences in rows, samples in columns.
+###2. Accession/OTU map. tab-delmited file from, for example, vsearch. two or more columns. first column is the OTUs which were retainend during clustering, each subsequent column contains either nothing, or one or more OTUs that should be folded into the OTU in the first column
+###3. Complete taxonomy assignments for accessions/OTUs #OPTIONAL
 
 ##output:
-###1. MED MATRIX-COUNT file, with inherited accessions and taxonomies
+###1. matrix similar to input 1, but now with fewer OTUs owing to some being absorbed
 
 #########
 ##USAGE##
 #########
-#python /path/to/inherit_accessions.py -i MATRIX-COUNT.transposed.txt -m NODE-REPRESENTATIVES.DOWNSTREAM_otus.txt -t complete_taxonomy_assignments.txt -o MATRIX-COUNT.transposed.inherited_accessions.txt
+#python /path/to/inherit_accessions.py -i MATRIX-COUNT.transposed.txt -m NODE-REPRESENTATIVES.DOWNSTREAM_otus.txt -o MATRIX-COUNT.transposed.inherited_accessions.txt
 
 print "run: python inherit_accessions.py -h for help."
 print "\n"
@@ -35,7 +35,7 @@ parser = argparse.ArgumentParser(
 requiredargs = parser.add_argument_group("required arguments")
 requiredargs.add_argument( #matrix file. user defined.
 	"-i",
-	"--matrix_count",
+	"--seq_table",
 	help = "Path to modified MED output file MATRIX-COUNT.transposed.txt. As name suggests, must be transposed, and also have leading zeroes removed from OTU IDs.",
 	required = True)
 requiredargs.add_argument( #path to otu map. user defined.
@@ -43,11 +43,11 @@ requiredargs.add_argument( #path to otu map. user defined.
 	"--otu_map",
 	help = "The OTU map produced from closed-reference OTU picking with MED nodes against the SILVA database.",
 	required = True)
-requiredargs.add_argument( #path to otu map. user defined.
-	"-t",
-	"--taxonomy_assignments",
-	help = "The taxonomies inherited and assigned in the previous steps of the pipeline. Must be tab-delmited with the first column as accessions or OTU IDs, and the second column as the taxonomy string. Confidence values or number of DB hits considered will not be retained.",
-	required = True)
+# requiredargs.add_argument( #path to otu map. user defined.
+# 	"-t",
+# 	"--taxonomy_assignments",
+# 	help = "The taxonomies inherited and assigned in the previous steps of the pipeline. Must be tab-delmited with the first column as accessions or OTU IDs, and the second column as the taxonomy string. Confidence values or number of DB hits considered will not be retained.",
+# 	required = True)
 parser.add_argument( #output file name. user defined. optional.
 	"-o",
 	"--outputfile",
@@ -57,7 +57,7 @@ args = parser.parse_args()
 
 matrix_count = args.matrix_count
 otu_mapfile = args.otu_map
-taxonomy=args.taxonomy_assignments
+# taxonomy=args.taxonomy_assignments
 outputfile = args.outputfile
 
 #read OTU map
@@ -91,14 +91,14 @@ with open (matrix_count) as MATRIXFILE: #open matrix count file
 	                matrix[OTU] = counts #link counts with OTUs
 MATRIXFILE.close()
 
-taxa = {}
-#read taxonomy assignments
-with open (taxonomy) as TAXONOMYFILE:
-        for i, line in enumerate(TAXONOMYFILE):
-                data = line.rstrip() #strip whitespace
-                all = data.split('\t') #split on tab
-                taxa[all[0]] = all[1] #make dictionary of IDs to taxa strings (first and second element of each row, respectively)
-TAXONOMYFILE.close()
+# taxa = {}
+# #read taxonomy assignments
+# with open (taxonomy) as TAXONOMYFILE:
+#         for i, line in enumerate(TAXONOMYFILE):
+#                 data = line.rstrip() #strip whitespace
+#                 all = data.split('\t') #split on tab
+#                 taxa[all[0]] = all[1] #make dictionary of IDs to taxa strings (first and second element of each row, respectively)
+# TAXONOMYFILE.close()
 
 #now merge OTUs that were assigned the same accession
 accession_counts = {}
@@ -121,17 +121,17 @@ try:
         OUTFILE=open(outputfile, "w")
 except AttributeError:
         outputfile=matrix_count[:-4] #remove extension (this depends on the file extension being 3 characters, plus a period delimiter. since these files are always going to be .txt files this should be fine for now.
-        completename=os.path.join(outputfile + ".inherited_accessions.txt")
+        completename=os.path.join(outputfile + ".100_sim_collapsed.txt")
         OUTFILE=open(completename, "w")
 headerprint = "\t".join(header)
-OUTFILE.write(headerprint + "\t" + "taxonomy" + "\n") #write the header of the file, taxonomy column goes last.
+OUTFILE.write(headerprint + "\n") #write the header of the file
 for key in sorted(accession_counts): #for each accession
         toprint='\t'.join(str(x) for x in accession_counts[key]) #must coerce to str from int (needed int to do math with numpy above)
-        OUTFILE.write(key + "\t" + toprint + "\t" + taxa[key] + "\n") #print read counts and taxonomy string to file
+        OUTFILE.write(key + "\t" + toprint + "\n") #print read counts and taxonomy string to file
 
 for key in sorted(matrix): #for all OTU IDs we started with
         if key not in otus_merged: #if we didn't merge the OTU into an accession
                 toprint='\t'.join(str(x) for x in matrix[key])
-                OUTFILE.write(key + "\t" + toprint + "\t" + taxa[key] + "\n") #print read counts and taxonomy string to file
+                OUTFILE.write(key + "\t" + toprint + "\n") #print read counts and taxonomy string to file
         else: #otherwise don't do anything (this skips merged OTUs, whose read counts were merged already)
              pass
