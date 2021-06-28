6f4c2a519ef12036a1d139f2f61c73fa5664dc1b
morien <morien@users.noreply.github.com>
2021-06-28 16:49:13 -0700

    Update assign_taxonomy.all_amplicons.BLAST.sh

--- assign_taxonomy.all_amplicons.BLAST.sh
+++ assign_taxonomy.all_amplicons.BLAST.sh
@@ -1,6 +1,6 @@
 #code for producing taxonomic assignments using blast, for CO1 and 12S amplicon data
 #author: Evan Morien
-#last modified: June 23rd, 2021
+#last modified: June 28th, 2021
 
 #Introduction
 #	The following code/pipeline was developed over the course of 2020/2021 to use blast to generate taxonomic assignments for both CO1 and 12S amplicon sequencing experiments
@@ -20,7 +20,7 @@ mkdir blast_96_sim
 blastn -task megablast -num_threads 38 -evalue 1e-5 -max_target_seqs 10 -perc_identity 96 -qcov_hsp_perc 50 -db ~/projects/taxonomyDBs/NCBI_NT/2020_08_28/blastdb/nt -outfmt '6 qseqid stitle sacc staxid pident qcovs evalue bitscore' -query 12S_ASV_sequences.length_var.fasta  -out blast_96_sim/12S_ASV_sequences.length_var.blast.out
 python2 ~/programs/galaxy-tool-BLAST/blastn_add_taxonomy_lite.py -i blast_96_sim/12S_ASV_sequences.length_var.blast.out -t ~/programs/Simple-LCA/rankedlineage.dmp -m ~/programs/Simple-LCA/merged.dmp -o blast_96_sim/taxonomy
 cat <(head -n 1 ~/programs/galaxy-tool-lca/example/example.tabular) taxonomy_12S_ASV_sequences.length_var.blast.out > tmp
-python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o blast_96_sim/taxonomy_table.12S.NCBI_NT.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified -flh unclassified,unknown
+python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o blast_96_sim/taxonomy_table.12S.NCBI_NT.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified,kingdom,phylum,class,order,family,genus -flh unclassified,unknown
 
 #cleanup
 rm blast_96_sim/12S_ASV_sequences.length_var.blast.out #remove blast output without taxonomy
@@ -108,7 +108,7 @@ sed -i 's/unknown kingdom \/ unknown phylum \/ unknown class \/ Telonemida/Telon
 sed -i 's/unknown kingdom \/ unknown phylum \/ unknown class \/ Jakobida/Jakobida \/ Jakobida \/ Jakobida \/ Jakobida/g' tmp #Jakobida
 
 #execute final step for the LCA program (forming consensus taxonomies for each ASV)
-python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o blast_96_sim/taxonomy_table.CO1.NCBI_NT.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified,kingdom -flh unclassified,unknown
+python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o -o blast_96_sim/taxonomy_table.CO1.NCBI_NT+customDB.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified,kingdom,phylum,class,order,family,genus -flh unclassified,unknown
 
 #cleanup
 mv tmp blast_96_sim/taxonomy_CO1_ASV_sequences.combined.blast.out #replace with taxonomy added blast output
