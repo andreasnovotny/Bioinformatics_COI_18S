95d46a5f7ff9d9c379dc02225140e25d7fb1fa61
morien <morien@users.noreply.github.com>
2022-04-26 17:52:15 -0700

    updating 18S blast procedure

--- assign_taxonomy.all_amplicons.BLAST.sh
+++ assign_taxonomy.all_amplicons.BLAST.sh
@@ -13,7 +13,7 @@
 java -Xmx248g -jar ~/programs/rdp_classifier_2.13/dist/classifier.jar classify -c 0.8 -t ~/projects/taxonomyDBs/12S_database/terrimporter_12S_fish_classifier/mydata_trained/rRNAClassifier.properties -o taxonomy_table.12S.merged.RDP.txt 12S_ASV_sequences.length_var.fasta
 
 
-#### 12S, 16S, or 18S blasting against NCBI NT ####
+#### 12S or 16S blasting against NCBI NT ####
 #remember to change the input and output file names so they match the amplicon you're working with
 #assign taxonomy with blast NT database at 96% similarity threshold
 mkdir blast_96_sim
@@ -29,6 +29,50 @@ python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o blast_96_sim/taxonomy_table.
 rm blast_96_sim/12S_ASV_sequences.length_var.blast.out #remove blast output without taxonomy
 rm taxonomy_12S_ASV_sequences.length_var.blast.out #remove redundant file
 mv tmp blast_96_sim/12S_ASV_sequences.length_var.blast.out #replace with taxonomy added blast output
+
+
+
+#### 18S amplicon blast with NCBI SSU database ####
+mkdir blast_96_sim
+blastn -task megablast -num_threads 38 -evalue 1e-5 -max_target_seqs 10 -perc_identity 96 -qcov_hsp_perc 50 -db ~/projects/taxonomyDBs/NCBI_SSU_euks/SSU_eukaryote_rRNA -outfmt '6 qseqid stitle sacc staxid pident qcovs evalue bitscore' -query 18S_ASV_sequences.fasta  -out blast_96_sim/18S_ASV_sequences.NCBI_SSU.blast.out
+python2 ~/programs/galaxy-tool-BLAST/blastn_add_taxonomy_lite.py -i blast_96_sim/18S_ASV_sequences.NCBI_SSU.blast.out -t ~/projects/taxonomyDBs/NCBI_taxonomy/2021-11-05/rankedlineage.dmp -m ~/projects/taxonomyDBs/NCBI_taxonomy/2021-11-05/merged.dmp -o taxonomy
+cat <(head -n 1 ~/programs/galaxy-tool-lca/example/example.tabular) taxonomy_18S_ASV_sequences.NCBI_SSU.blast.out > tmp
+
+#label fix for clades missing "kingdom" label
+sed -i 's/unknown kingdom \/ Bacillariophyta/Eukaryota \/ Bacillariophyta/g' tmp #Bacillariophyta
+sed -i 's/unknown kingdom \/ Ciliophora/Eukaryota \/ Ciliophora/g' tmp #Ciliophora
+sed -i 's/unknown kingdom \/ Discosea/Eukaryota \/ Discosea/g' tmp #Discosea
+sed -i 's/unknown kingdom \/ Evosea/Eukaryota \/ Evosea/g' tmp #Evosea
+sed -i 's/unknown kingdom \/ Haptista/Eukaryota \/ Haptista/g' tmp #Haptista
+sed -i 's/unknown kingdom \/ Rhodophyta/Eukaryota \/ Rhodophyta/g' tmp #Rhodophyta
+
+#and for those missing kingdom + phylum labels
+sed -i 's/Eukaryota \/ unknown phylum \/ Chrysophyceae/Eukaryota \/ Chrysophyceae \/ Chrysophyceae/g' tmp #Chrysophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Cryptophyceae/Eukaryota \/ Cryptophyceae \/ Cryptophyceae/g' tmp #Cryptophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Oomycota/Eukaryota \/ Oomycota \/ Oomycota/g' tmp #Oomycota
+sed -i 's/Eukaryota \/ unknown phylum \/ Phaeophyceae/Eukaryota \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Phaeophyceae/Eukaryota \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Bigyra/Eukaryota \/ Bigyra \/ Bigyra/g' tmp #Bigyra
+sed -i 's/Eukaryota \/ unknown phylum \/ Dictyochophyceae/Eukaryota \/ Dictyochophyceae \/ Dictyochophyceae/g' tmp #Dictyochophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Dinophyceae/Eukaryota \/ Dinophyceae \/ Dinophyceae/g' tmp #Dinophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Pelagophyceae/Eukaryota \/ Pelagophyceae \/ Pelagophyceae/g' tmp #Pelagophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Raphidophyceae/Eukaryota \/ Raphidophyceae \/ Raphidophyceae/g' tmp #Raphidophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Synurophyceae/Eukaryota \/ Synurophyceae \/ Synurophyceae/g' tmp #Synurophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Bolidophyceae/Eukaryota \/ Bolidophyceae \/ Bolidophyceae/g' tmp #Bolidophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Polycystinea/Eukaryota \/ Polycystinea \/ Polycystinea/g' tmp #Polycystinea
+sed -i 's/Eukaryota \/ unknown phylum \/ Choanoflagellata/Eukaryota \/ Choanoflagellata \/ Choanoflagellata/g' tmp #Choanoflagellata
+sed -i 's/Eukaryota \/ unknown phylum \/ Filasterea/Eukaryota \/ Filasterea \/ Filasterea/g' tmp #Filasterea
+
+#label for those missing kindom + phylum + class labels
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Telonemida/Eukaryota \/ Telonemida \/ Telonemida \/ Telonemida/g' tmp #Telonemida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Jakobida/Eukaryota \/ Jakobida \/ Jakobida \/ Jakobida/g' tmp #Jakobida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Pirsoniales/Eukaryota \/ Pirsoniales \/ Pirsoniales \/ Pirsoniales/g' tmp #Pirsoniales
+
+#for 18S, i have found that the filtering parameters below are best for identifying bacterial (non 18S sequence)
+python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o taxonomy_table.18S.NCBI_SSU.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified,kingdom -flh unclassified
+
+#cleanup
+rm taxonomy_18S_ASV_sequences.NCBI_SSU.blast.out tmp
 
 
 #### CO1 amplicon blast with NT and custom CO1 sequence database ####
@@ -86,29 +130,34 @@ cat <(head -n 1 ~/programs/galaxy-tool-lca/example/example.tabular) taxonomy_tmp
 # also note, "unknown phylum" is present in any taxonomy where the clade's phylum is uncertain in the NCBI taxonomy system, it doesn't indicate any other kind of uncertainty about the provenance of the sequence.
 
 #label fix for clades missing "kingdom" label
-sed -i 's/unknown kingdom \/ Bacillariophyta/Bacillariophyta \/ Bacillariophyta/g' tmp #Bacillariophyta
-sed -i 's/unknown kingdom \/ Ciliophora/Ciliophora \/ Ciliophora/g' tmp #Ciliophora
-sed -i 's/unknown kingdom \/ Discosea/Discosea \/ Discosea/g' tmp #Discosea
-sed -i 's/unknown kingdom \/ Evosea/Evosea \/ Evosea/g' tmp #Evosea
-sed -i 's/unknown kingdom \/ Haptista/Haptista \/ Haptista/g' tmp #Haptista
-sed -i 's/unknown kingdom \/ Rhodophyta/Rhodophyta \/ Rhodophyta/g' tmp #Rhodophyta
+sed -i 's/unknown kingdom \/ Bacillariophyta/Eukaryota \/ Bacillariophyta/g' tmp #Bacillariophyta
+sed -i 's/unknown kingdom \/ Ciliophora/Eukaryota \/ Ciliophora/g' tmp #Ciliophora
+sed -i 's/unknown kingdom \/ Discosea/Eukaryota \/ Discosea/g' tmp #Discosea
+sed -i 's/unknown kingdom \/ Evosea/Eukaryota \/ Evosea/g' tmp #Evosea
+sed -i 's/unknown kingdom \/ Haptista/Eukaryota \/ Haptista/g' tmp #Haptista
+sed -i 's/unknown kingdom \/ Rhodophyta/Eukaryota \/ Rhodophyta/g' tmp #Rhodophyta
 
 #and for those missing kingdom + phylum labels
-sed -i 's/unknown kingdom \/ unknown phylum \/ Chrysophyceae/Chrysophyceae \/ Chrysophyceae \/ Chrysophyceae/g' tmp #Chrysophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Cryptophyceae/Cryptophyceae \/ Cryptophyceae \/ Cryptophyceae/g' tmp #Cryptophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Oomycota/Oomycota \/ Oomycota \/ Oomycota/g' tmp #Oomycota
-sed -i 's/unknown kingdom \/ unknown phylum \/ Phaeophyceae/Phaeophyceae \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Phaeophyceae/Phaeophyceae \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Bigyra/Bigyra \/ Bigyra \/ Bigyra/g' tmp #Bigyra
-sed -i 's/unknown kingdom \/ unknown phylum \/ Dictyochophyceae/Dictyochophyceae \/ Dictyochophyceae \/ Dictyochophyceae/g' tmp #Dictyochophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Dinophyceae/Dinophyceae \/ Dinophyceae \/ Dinophyceae/g' tmp #Dinophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Pelagophyceae/Pelagophyceae \/ Pelagophyceae \/ Pelagophyceae/g' tmp #Pelagophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Raphidophyceae/Raphidophyceae \/ Raphidophyceae \/ Raphidophyceae/g' tmp #Raphidophyceae
-sed -i 's/unknown kingdom \/ unknown phylum \/ Synurophyceae/Synurophyceae \/ Synurophyceae \/ Synurophyceae/g' tmp #Synurophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Chrysophyceae/Eukaryota \/ Chrysophyceae \/ Chrysophyceae/g' tmp #Chrysophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Cryptophyceae/Eukaryota \/ Cryptophyceae \/ Cryptophyceae/g' tmp #Cryptophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Oomycota/Eukaryota \/ Oomycota \/ Oomycota/g' tmp #Oomycota
+sed -i 's/Eukaryota \/ unknown phylum \/ Phaeophyceae/Eukaryota \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Phaeophyceae/Eukaryota \/ Phaeophyceae \/ Phaeophyceae/g' tmp #Phaeophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Bigyra/Eukaryota \/ Bigyra \/ Bigyra/g' tmp #Bigyra
+sed -i 's/Eukaryota \/ unknown phylum \/ Dictyochophyceae/Eukaryota \/ Dictyochophyceae \/ Dictyochophyceae/g' tmp #Dictyochophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Dinophyceae/Eukaryota \/ Dinophyceae \/ Dinophyceae/g' tmp #Dinophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Pelagophyceae/Eukaryota \/ Pelagophyceae \/ Pelagophyceae/g' tmp #Pelagophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Raphidophyceae/Eukaryota \/ Raphidophyceae \/ Raphidophyceae/g' tmp #Raphidophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Synurophyceae/Eukaryota \/ Synurophyceae \/ Synurophyceae/g' tmp #Synurophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Bolidophyceae/Eukaryota \/ Bolidophyceae \/ Bolidophyceae/g' tmp #Bolidophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Polycystinea/Eukaryota \/ Polycystinea \/ Polycystinea/g' tmp #Polycystinea
+sed -i 's/Eukaryota \/ unknown phylum \/ Choanoflagellata/Eukaryota \/ Choanoflagellata \/ Choanoflagellata/g' tmp #Choanoflagellata
+sed -i 's/Eukaryota \/ unknown phylum \/ Filasterea/Eukaryota \/ Filasterea \/ Filasterea/g' tmp #Filasterea
 
 #label for those missing kindom + phylum + class labels
-sed -i 's/unknown kingdom \/ unknown phylum \/ unknown class \/ Telonemida/Telonemida \/ Telonemida \/ Telonemida \/ Telonemida/g' tmp #Telonemida
-sed -i 's/unknown kingdom \/ unknown phylum \/ unknown class \/ Jakobida/Jakobida \/ Jakobida \/ Jakobida \/ Jakobida/g' tmp #Jakobida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Telonemida/Eukaryota \/ Telonemida \/ Telonemida \/ Telonemida/g' tmp #Telonemida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Jakobida/Eukaryota \/ Jakobida \/ Jakobida \/ Jakobida/g' tmp #Jakobida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Pirsoniales/Eukaryota \/ Pirsoniales \/ Pirsoniales \/ Pirsoniales/g' tmp #Pirsoniales
 
 #execute final step for the LCA program (forming consensus taxonomies for each ASV)
 python2 ~/programs/galaxy-tool-lca/lca.py -i tmp -o blast_96_sim/taxonomy_table.CO1.NCBI_NT+customDB.96sim.txt -b 100 -id 96 -cov 50 -t best_hit -tid 98 -tcov 80 -fh environmental,unidentified,kingdom,phylum,class,order,family,genus -flh unclassified,unknown
@@ -206,10 +255,15 @@ sed -i 's/Eukaryota \/ unknown phylum \/ Dinophyceae/Eukaryota \/ Dinophyceae \/
 sed -i 's/Eukaryota \/ unknown phylum \/ Pelagophyceae/Eukaryota \/ Pelagophyceae \/ Pelagophyceae/g' tmp #Pelagophyceae
 sed -i 's/Eukaryota \/ unknown phylum \/ Raphidophyceae/Eukaryota \/ Raphidophyceae \/ Raphidophyceae/g' tmp #Raphidophyceae
 sed -i 's/Eukaryota \/ unknown phylum \/ Synurophyceae/Eukaryota \/ Synurophyceae \/ Synurophyceae/g' tmp #Synurophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Bolidophyceae/Eukaryota \/ Bolidophyceae \/ Bolidophyceae/g' tmp #Bolidophyceae
+sed -i 's/Eukaryota \/ unknown phylum \/ Polycystinea/Eukaryota \/ Polycystinea \/ Polycystinea/g' tmp #Polycystinea
+sed -i 's/Eukaryota \/ unknown phylum \/ Choanoflagellata/Eukaryota \/ Choanoflagellata \/ Choanoflagellata/g' tmp #Choanoflagellata
+sed -i 's/Eukaryota \/ unknown phylum \/ Filasterea/Eukaryota \/ Filasterea \/ Filasterea/g' tmp #Filasterea
 
 #label for those missing kindom + phylum + class labels
 sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Telonemida/Eukaryota \/ Telonemida \/ Telonemida \/ Telonemida/g' tmp #Telonemida
 sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Jakobida/Eukaryota \/ Jakobida \/ Jakobida \/ Jakobida/g' tmp #Jakobida
+sed -i 's/Eukaryota \/ unknown phylum \/ unknown class \/ Pirsoniales/Eukaryota \/ Pirsoniales \/ Pirsoniales \/ Pirsoniales/g' tmp #Pirsoniales
 
 #execute final step for the LCA program (forming consensus taxonomies for each ASV)
 mv -f tmp blast_96_sim/CO1_ASV_sequences.combined_all.blast.out #put tmp file where it belongs, add label (this is an overwrite, careful!!)
